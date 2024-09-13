afc4b8d149f495c223feb5df77a61490a42ae9d0
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2024-09-13 19:53:40 -0300

    Add files via upload

--- Desafio 5/Desafio 5 - Codigos/src/Application/EmbaralhadorPalavras.java
+++ Desafio 5/Desafio 5 - Codigos/src/Application/EmbaralhadorPalavras.java
@@ -8,11 +8,6 @@ public class EmbaralhadorPalavras {
 
     public String embaralharPalavras(String palavra) {
 
-        //O erro se encontra no método trim!
-        //List<String> letras = Arrays.asList(palavra.trim());
-        // Este método retorna uma string com espaços em branco removidos.
-        // O método correto é o split, que divide esta string em torno das 
-        // correspondências da expressão regular fornecida, que neste caso é o "".     
         List<String> letras = Arrays.asList(palavra.split(""));
         Collections.shuffle(letras);
 
